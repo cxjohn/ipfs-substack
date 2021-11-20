f342418a6c7a90c7f5a2213a2c37b41168913ff5
Chris John <csjohn@shaw.ca>
2021-11-19 20:53:13 -0500

    connecting form elements and adding validation

--- pages/publish.js
+++ pages/publish.js
@@ -1,22 +1,33 @@
 import Head from "next/head";
 import { useState } from "react";
 import dynamic from "next/dynamic";
-import { useForm } from "react-hook-form";
 import NavBar from "./../src/layout/Navbar";
+import { EditorState, convertToRaw } from "draft-js";
+import draftToHtml from "draftjs-to-html";
+import { useForm } from "react-hook-form";
 
 const Prose = dynamic(() => import("./../src/components/Prose.tsx"), {
   ssr: false,
 });
 
 export default function Publish() {
-  const [proseState, setProseState] = useState("");
+  const [editorState, setEditorState] = useState(EditorState.createEmpty());
+  const onEditorStateChange = (editorState) => {
+    setEditorState(editorState);
+  };
+
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm();
-  const onSubmit = (data) => console.log(data);
-  console.log(errors);
+  } = useForm({ mode: "onChange" });
+
+  const onSubmit = (data) => {
+    data.body = draftToHtml(
+      convertToRaw(editorState.getCurrentContent())
+    );
+    return console.log(data);
+  };
 
   return (
     <>
@@ -45,22 +56,31 @@ export default function Publish() {
               />
             </div>
 
-            <Prose />
+            <Prose
+              editorState={editorState}
+              onEditorStateChange={onEditorStateChange}
+            />
             <div className="flex justify-between items-center w-full">
               <label className="relative flex justify-between items-center group p-2 text-lg">
                 Paid subscribers only?
                 <input
                   type="checkbox"
                   className="absolute left-1/2 -translate-x-1/2 w-full h-full peer appearance-none rounded-md"
+                  {...register("paid", { required: false })}
                 />
                 <span className="w-16 h-10 flex items-center flex-shrink-0 ml-4 p-1 bg-gray-300 rounded-full duration-300 ease-in-out peer-checked:bg-indigo-600 after:w-8 after:h-8 after:bg-white after:rounded-full after:shadow-md after:duration-300 peer-checked:after:translate-x-6 group-hover:after:translate-x-1"></span>
               </label>
-
-              <input
-                className="bg-indigo-600 hover:bg-indigo-500 text-white px-5 h-10 rounded-md cursor-pointer transition-colors duration-200 ease-in-out transform"
-                type="submit"
-                value="Publish"
-              />
+              <div className="flex">
+                <div className="flex flex-col text-red-600 text-sm">
+                  <span>{errors.title && "Please enter a title"}</span>
+                  <span>{errors.subtitle && "Please enter a subtitle"}</span>
+                </div>
+                <input
+                  className="bg-indigo-600 hover:bg-indigo-500 text-white px-5 h-10 rounded-md cursor-pointer transition-colors duration-200 ease-in-out transform ml-2"
+                  type="submit"
+                  value="Publish"
+                />
+              </div>
             </div>
           </form>
         </div>
--- src/components/Prose.tsx
+++ src/components/Prose.tsx
@@ -1,58 +1,22 @@
-import React, { Component } from "react";
-import { EditorState, convertToRaw, ContentState } from "draft-js";
 import { Editor } from "react-draft-wysiwyg";
-import draftToHtml from "draftjs-to-html";
-import htmlToDraft from "html-to-draftjs";
-
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 
-export default class EditorConvertToHTML extends Component {
-  constructor(props) {
-    super(props);
-    const html = "<p>Hey this <strong>editor</strong> rocks 😀</p>";
-    const contentBlock = htmlToDraft(html);
-    if (contentBlock) {
-      const contentState = ContentState.createFromBlockArray(
-        contentBlock.contentBlocks
-      );
-      const editorState = EditorState.createWithContent(contentState);
-      this.state = {
-        editorState,
-      };
-    }
-  }
-
-  onEditorStateChange: Function = (editorState) => {
-    this.setState({
-      editorState,
-    });
-  };
-
-  render() {
-    const { editorState } = this.state;
-    return (
-      <div>
-        <Editor
-          editorState={editorState}
-          wrapperClassName="demo-wrapper"
-          editorClassName="!h-72"
-          toolbarClassName="!border-t-0 !border-l-0 !border-r-0"
-          toolbar={{
-            fontFamily: { options: [], className: "hidden" },
-            inline: { options: ['bold', 'italic'] },
-            list: { inDropdown: true },
-            embedded: { className: "!hidden" },
-            textAlign: { inDropdown: true },
-            
-          }}
-          onEditorStateChange={this.onEditorStateChange}
-        />
-        {/* <textarea
-          className="h-72"
-          disabled
-          value={draftToHtml(convertToRaw(editorState.getCurrentContent()))}
-        /> */}
-      </div>
-    );
-  }
+export default function Summerform({ editorState, onEditorStateChange }) {
+  return (
+    <>
+      <Editor
+        editorState={editorState}
+        editorClassName="!min-h-[400px] border-b"
+        toolbarClassName="!border-t-0 !border-l-0 !border-r-0"
+        toolbar={{
+          fontFamily: { options: [], className: "hidden" },
+          inline: { options: ["bold", "italic"] },
+          list: { inDropdown: true },
+          embedded: { className: "!hidden" },
+          textAlign: { inDropdown: true },
+        }}
+        onEditorStateChange={onEditorStateChange}
+      />
+    </>
+  );
 }
