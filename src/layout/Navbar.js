2f8f343a8c152c54ad2a635f9f52408b995b05d8
CalebJohn <calebjohn4@gmail.com>
2021-11-21 11:24:09 -0800

    Increase gas, limit call to slice

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -17,7 +17,7 @@ export default function NavBar({ }) {
           <div className="flex items-center justify-between">
             {router.pathname === "/dashboard" ? (
 							<div className="flex cursor-pointer" onClick={() => {navigator.clipboard.writeText(window.did)}}>
-							{`Welcome ${window.did.slice(0, 16)}...`} <svg class="h-4 w-4 text-gray-500"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <rect x="8" y="8" width="12" height="12" rx="2" />  <path d="M16 8v-2a2 2 0 0 0 -2 -2h-8a2 2 0 0 0 -2 2v8a2 2 0 0 0 2 2h2" /></svg>
+							{`Welcome ${window.did?.slice(0, 16)}...`} <svg class="h-4 w-4 text-gray-500"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <rect x="8" y="8" width="12" height="12" rx="2" />  <path d="M16 8v-2a2 2 0 0 0 -2 -2h-8a2 2 0 0 0 -2 2v8a2 2 0 0 0 2 2h2" /></svg>
 							</div>
             ) : (
               <Link href="/dashboard">
